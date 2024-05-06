20deb68527ce433862a0c084b92a1ad3ed8c665e
felix-wj <1358726405@qq.com>
2024-05-06 18:27:06 +0800

    :tada:

---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="account">
+        <div class="account-container">
+            <div class="login-form">
+                <div class="account-top">
+                    <div class="account-top-logo">
+                        <span>用户登录</span>
+                    </div>
+                    <div class="account-top-desc">横看成峰侧成岭 远近高低各不同</div>
+                </div>
+                <!-- 表单 -->
+                <a-form>
+                    <a-form-item field="name" tooltip="请输入用户名" label="Username">
+                        <a-input v-model="form.name" placeholder="请输入用户名..."/>
+                    </a-form-item>
+                </a-form>
+            </div>
+        </div>
+    </div>
+</template>
